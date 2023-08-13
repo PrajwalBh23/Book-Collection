7efb4abac2c188dc73dd1690dd7af3c8a7a58130
Hemant Kumbhalkar <hemantkumbhalkar5@gmail.com>
2023-08-13 22:22:00 +0530

    Added some logic to solve error issue

--- Backned/controllers/controller.js
+++ Backned/controllers/controller.js
@@ -1,27 +1,28 @@
-import Book from '../model/Book.js';
+import Book from "../model/Book.js";
 import multer from "multer";
+import fs from "fs";
 
 export const getAllBooks = async (req, res, next) => {
-    let books;
-    try {
-        books = await Book.find();
-    } catch (err) {
-        console.log(err);
-    }
+  let books;
+  try {
+    books = await Book.find();
+  } catch (err) {
+    console.log(err);
+  }
 
-    if (!books) {
-        return res.status(404).json({ message: "No message found" });
-    }
-    return res.status(200).json({ books });
-}
+  if (!books) {
+    return res.status(404).json({ message: "No message found" });
+  }
+  return res.status(200).json({ books });
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads'); // Specify the directory to save uploaded files
+    cb(null, "./public/uploads");
   },
   filename: function (req, file, cb) {
     const originalFileName = file.originalname;
-    cb(null, originalFileName); // Save the file with the original filename
+    cb(null, originalFileName);
   },
 });
 
@@ -29,16 +30,16 @@ const upload = multer({ storage: storage });
 
 export const addBooks = async (req, res, next) => {
   try {
-    upload.single('image')(req, res, async function (err) {
+    upload.single("image")(req, res, async function (err) {
       if (err instanceof multer.MulterError) {
-        return res.status(400).json({ message: 'File upload error' });
+        return res.status(400).json({ message: "File upload error" });
       } else if (err) {
-        console.log(err); // Log the error for debugging
-        return res.status(500).json({ message: 'Internal server error' });
+        console.log(err);
+        return res.status(500).json({ message: "Internal server error" });
       }
 
       const { name, author, description, price, available } = req.body;
-      const imageFilename = req.file.filename; 
+      const imageBuffer = fs.readFileSync(req.file.path);
 
       const book = new Book({
         name,
@@ -46,74 +47,77 @@ export const addBooks = async (req, res, next) => {
         description,
         price,
         available,
-        image: imageFilename, 
+        image: imageBuffer,
       });
 
       try {
-        // Save the book to the database
         const savedBook = await book.save();
         return res.status(201).json({ book: savedBook });
       } catch (validationError) {
         console.log(validationError);
-        return res.status(400).json({ message: 'Validation error', errors: validationError.errors });
+        return res.status(400).json({
+          message: "Validation error",
+          errors: validationError.errors,
+        });
       }
     });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ message: 'Internal server error' });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const getbyID = async (req, res, next) => {
-    const id = req.params.id;
-    let book;
-    try {
-        book = await Book.findById(id);
-    }
-    catch (err) {
-        console.log(err);
-    }
-    if (!book) {
-        return res.status(500).json({ message: "No book Found" });
-    }
-    return res.json({ book });
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findById(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(500).json({ message: "No book Found" });
+  }
+  return res.json({ book });
 };
 
 export const updateBook = async (req, res, next) => {
-    const id = req.params.id;
-    const { name, author, description, price, available, image } = req.body;
-    let book;
-    try {
-        book = await Book.findByIdAndUpdate(id, {
-            name,
-            author,
-            description,
-            price,
-            available,
-            image
-        }, { new: true });
-        book = await book.save();
-    }
-    catch (err) {
-        console.log(err);
-    }
-    if (!book) {
-        return res.status(404).json({ message: "Unable to update" });
-    }
-    return res.status(200).json({ book });
-}
+  const id = req.params.id;
+  const { name, author, description, price, available, image } = req.body;
+  let book;
+  try {
+    book = await Book.findByIdAndUpdate(
+      id,
+      {
+        name,
+        author,
+        description,
+        price,
+        available,
+        image,
+      },
+      { new: true }
+    );
+    book = await book.save();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "Unable to update" });
+  }
+  return res.status(200).json({ book });
+};
 
 export const delectBook = async (req, res, next) => {
-    const id = req.params.id;
-    let book;
-    try {
-        book = await Book.findByIdAndRemove(id);
-    }
-    catch (err) {
-        console.log(err);
-    }
-    if (!book) {
-        return res.status(404).json({ message: "Unable to update" });
-    }
-    return res.status(200).json({ message: "Successsfully removed" });
-}
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findByIdAndRemove(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "Unable to update" });
+  }
+  return res.status(200).json({ message: "Successsfully removed" });
+};
--- frontned/src/components/Addbook.js
+++ frontned/src/components/Addbook.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
-import { Button, Box, FormLabel, TextField, FormControlLabel, Checkbox } from "@mui/material"
+import React, { useState } from "react";
+import {
+  Button,
+  Box,
+  FormLabel,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const AddBook = () => {
   const history = useNavigate();
   const [inputField, setInputField] = useState({
-    name: '',
-    author: '',
-    description: '',
-    price: '',
-    image: null, // Initialize image as null
+    name: "",
+    author: "",
+    description: "",
+    price: "",
+    image: null,
   });
 
   const [checked, setChecked] = useState(false);
@@ -25,7 +32,7 @@ const AddBook = () => {
   const handleImageChange = (e) => {
     setInputField((prevInput) => ({
       ...prevInput,
-      image: e.target.files[0], // Update the image object
+      image: e.target.files[0],
     }));
   };
 
@@ -33,11 +40,12 @@ const AddBook = () => {
     e.preventDefault();
     const url = "http://localhost:5000/books";
     const formData = new FormData();
-    formData.append('name', inputField.name);
-    formData.append('author', inputField.author);
-    formData.append('description', inputField.description);
-    formData.append('price', inputField.price);
-    formData.append('image', inputField.image);
+    formData.append("name", inputField.name);
+    formData.append("author", inputField.author);
+    formData.append("description", inputField.description);
+    formData.append("price", inputField.price);
+    formData.append("available", checked);
+    formData.append("image", inputField.image);
 
     try {
       const response = await axios.post(url, formData);
@@ -50,33 +58,89 @@ const AddBook = () => {
     }
   };
 
-  return <form onSubmit={handleSubmit}>
-    <Box display="flex" flexDirection="column" justifyContent="center" maxWidth={700} alignContent="center" alignSelf="center" marginLeft={"auto"} marginRight={"auto"} marginTop={5}>
-      <FormLabel style={{ marginTop: '10px' }} margin='normal'>Name</FormLabel>
-      <TextField value={inputField.name} onChange={handleChange} fullWidth variant='outlined' name='name' />
-      <FormLabel style={{ marginTop: '10px' }} margin='normal'>Author</FormLabel>
-      <TextField value={inputField.author} onChange={handleChange} fullWidth variant='outlined' name='author' />
-      <FormLabel style={{ marginTop: '10px' }} margin='normal'>Description</FormLabel>
-      <TextField value={inputField.description} onChange={handleChange} fullWidth variant='outlined' name='description' />
-      <FormLabel style={{ marginTop: '10px' }} margin='normal'>Price</FormLabel>
-      <TextField value={inputField.price} onChange={handleChange} type='number' fullWidth variant='outlined' name='price' />
-      <FormLabel style={{ marginTop: '10px' }} margin='normal'>Image</FormLabel>
-      <input type="file" accept="image/*" name="image" onChange={handleImageChange} />
-      {inputField.image && (
-  <img src={URL.createObjectURL(inputField.image)} alt={`Cover of ${inputField.name}`}/>
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        maxWidth={700}
+        alignContent="center"
+        alignSelf="center"
+        marginLeft={"auto"}
+        marginRight={"auto"}
+        marginTop={5}
+      >
+        <FormLabel style={{ marginTop: "10px" }} margin="normal">
+          Name
+        </FormLabel>
+        <TextField
+          value={inputField.name}
+          onChange={handleChange}
+          fullWidth
+          variant="outlined"
+          name="name"
+        />
+        <FormLabel style={{ marginTop: "10px" }} margin="normal">
+          Author
+        </FormLabel>
+        <TextField
+          value={inputField.author}
+          onChange={handleChange}
+          fullWidth
+          variant="outlined"
+          name="author"
+        />
+        <FormLabel style={{ marginTop: "10px" }} margin="normal">
+          Description
+        </FormLabel>
+        <TextField
+          value={inputField.description}
+          onChange={handleChange}
+          fullWidth
+          variant="outlined"
+          name="description"
+        />
+        <FormLabel style={{ marginTop: "10px" }} margin="normal">
+          Price
+        </FormLabel>
+        <TextField
+          value={inputField.price}
+          onChange={handleChange}
+          type="number"
+          fullWidth
+          variant="outlined"
+          name="price"
+        />
+        <FormLabel style={{ marginTop: "10px" }} margin="normal">
+          Image
+        </FormLabel>
+        <input
+          type="file"
+          accept="image/*"
+          name="image"
+          onChange={handleImageChange}
+        />
+        {inputField.image && (
+          <img
+            src={URL.createObjectURL(inputField.image)}
+            alt={`Cover of ${inputField.name}`}
+          />
+        )}
 
-)}
+        <FormControlLabel
+          control={
+            <Checkbox checked={checked} onChange={() => setChecked(!checked)} />
+          }
+          label="Available"
+        />
 
-      <FormControlLabel
-        control={
-          <Checkbox checked={checked} onChange={() => setChecked(!checked)} />
-        }
-        label="Available"
-      />
-
-      <Button style={{ marginTop: '15px' }} variant="contained" type='submit'>Add Book</Button>
-    </Box>
-  </form>
-}
+        <Button style={{ marginTop: "15px" }} variant="contained" type="submit">
+          Add Book
+        </Button>
+      </Box>
+    </form>
+  );
+};
 
 export default AddBook;
